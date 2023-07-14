d05a4581c36e8294ca6e4c3a98ec1b0d742d0020
unknown <MSLadanov@gmail.com>
2023-07-14 20:17:58 +0300

    navbar

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,14 +5,23 @@ import "./style.scss";
 export const Header = () => {
   return (
     <nav>
-      <div className="logo"></div>
+      <div className="logo">
+        <img src="/images/logo.svg" alt="logo" />
+      </div>
       <div className="site-navbar">
         <NavLink to="men/">Men</NavLink>
         <NavLink to="women/">Women</NavLink>
         <NavLink to="about/">About</NavLink>
         <NavLink to="contact/">Contact</NavLink>
       </div>
-      <div className="account-navbar"></div>
+      <div className="account-navbar">
+        <div className="cart-button">
+          <img src="/images/icon-cart.svg" alt="cart" />
+        </div>
+        <div className="account-button">
+          <img src="/images/image-user.png" alt="user" />
+        </div>
+      </div>
     </nav>
   );
 };
