ae2f014c23266ca36a5956028922d50cbe27f1e5
unknown <MSLadanov@gmail.com>
2023-07-25 21:11:39 +0300

    sneaker page route created

--- src/components/SneakerPage/SneakerPage.tsx
+++ src/components/SneakerPage/SneakerPage.tsx
@@ -5,7 +5,6 @@ import axios from "axios";
 
 export const SneakerPage = () => {
   let { id, sex } = useParams();
-  console.log(sex)
   const [sneakerInfo, setSneakerInfo] = useState(null);
   const getSneakerInfo = async () => {
     const info = (await axios.get(`http://localhost:3001/api/sneaker/${id}`))
