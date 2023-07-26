56ea9af1de1e5c1a8d806d0941232754fe56c562
unknown <MSLadanov@gmail.com>
2023-07-26 18:58:52 +0300

    sneaker page creating

--- src/components/SneakerPage/SneakerPage.tsx
+++ src/components/SneakerPage/SneakerPage.tsx
@@ -5,17 +5,30 @@ import axios from "axios";
 
 export const SneakerPage = () => {
   let { id, sex } = useParams();
-  const [sneakerInfo, setSneakerInfo] = useState(null);
+  const [sneakerInfo, setSneakerInfo] = useState({ name: null, brand: null, description:null, price:null });
   const getSneakerInfo = async () => {
     const info = (await axios.get(`http://localhost:3001/api/sneaker/${id}`))
       .data as any;
-      if(info.sex.toLowerCase() === sex){
-        setSneakerInfo(info);
-      }
+    if (info.sex.toLowerCase() === sex) {
+      setSneakerInfo(info);
+    }
   };
   useEffect(() => {
     getSneakerInfo();
   }, []);
 
-  return <div>{JSON.stringify(sneakerInfo)}</div>;
+  return (
+    <div>
+      <div className="image-box">
+        <div className="main-image"></div>
+        <div className="thumbnails"></div>
+      </div>
+      <div className="info-box">
+        <h4>{sneakerInfo.brand}</h4>
+        <h1>{sneakerInfo.name}</h1>
+        <p>{sneakerInfo.description}</p>
+        <h2>{sneakerInfo.price} $</h2>
+      </div>
+    </div>
+  );
 };
