6ed3fb573ef58296c321d1af8f737abcd194d733
unknown <MSLadanov@gmail.com>
2023-07-27 18:37:35 +0300

    sneaker page fix

--- src/components/SneakerPage/SneakerPage.tsx
+++ src/components/SneakerPage/SneakerPage.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
+import ErrorPage from "../Routes/Error/Error";
 import './style.scss'
 
 export const SneakerPage = () => {
@@ -16,6 +17,7 @@ export const SneakerPage = () => {
     img2: null,
     img3: null,
     img4: null,
+    sizes: null
   });
   const getSneakerInfo = async () => {
     const info = (await axios.get(`http://localhost:3001/api/sneaker/${id}`))
@@ -27,48 +29,51 @@ export const SneakerPage = () => {
   useEffect(() => {
     getSneakerInfo();
   }, []);
-
-  return (
-    <div className="sneaker-page">
-      <div className="image-box">
-        <div className="main-image">
-          <img
-            src={`http://localhost:3001/${sneakerInfo.img1}`}
-            alt={`img1`}
-          />
-        </div>
-        <div className="thumbnails">
-          <div className="thumbnail"><img
-            src={`http://localhost:3001/${sneakerInfo.img1}`}
-            alt={`img1`}
-          /></div>
-          <div className="thumbnail"><img
-            src={`http://localhost:3001/${sneakerInfo.img2}`}
-            alt={`img1`}
-          /></div>
-          <div className="thumbnail"><img
-            src={`http://localhost:3001/${sneakerInfo.img3}`}
-            alt={`img1`}
-          /></div>
-          <div className="thumbnail"><img
-            src={`http://localhost:3001/${sneakerInfo.img4}`}
-            alt={`img1`}
-          /></div>
-        </div>
-      </div>
-      <div className="info-box">
-        <h4>{sneakerInfo.brand}</h4>
-        <h1>{sneakerInfo.name}</h1>
-        <p>{sneakerInfo.description}</p>
-        <div className="price-box">
-          <div className="price">
-            <h2>{sneakerInfo.price} $</h2>
+  if(sneakerInfo.name !== null){
+    return (
+      <div className="sneaker-page">
+        <div className="image-box">
+          <div className="main-image">
+            <img
+              src={`http://localhost:3001/${sneakerInfo.img1}`}
+              alt={`img1`}
+            />
           </div>
-          <div className="discount">
-            <h4>{sneakerInfo.discount} %</h4>
+          <div className="thumbnails">
+            <div className="thumbnail"><img
+              src={`http://localhost:3001/${sneakerInfo.img1}`}
+              alt={`img1`}
+            /></div>
+            <div className="thumbnail"><img
+              src={`http://localhost:3001/${sneakerInfo.img2}`}
+              alt={`img1`}
+            /></div>
+            <div className="thumbnail"><img
+              src={`http://localhost:3001/${sneakerInfo.img3}`}
+              alt={`img1`}
+            /></div>
+            <div className="thumbnail"><img
+              src={`http://localhost:3001/${sneakerInfo.img4}`}
+              alt={`img1`}
+            /></div>
           </div>
         </div>
+        <div className="info-box">
+          <h4>{sneakerInfo.brand}</h4>
+          <h1>{sneakerInfo.name}</h1>
+          <p>{sneakerInfo.description}</p>
+          <div className="price-box">
+            <div className="price">
+              <h2>{sneakerInfo.price} $</h2>
+            </div>
+            <div className="discount">
+              <h4>{sneakerInfo.discount} %</h4>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return (<div><h1>Not Found!</h1></div>)
+  }
 };
