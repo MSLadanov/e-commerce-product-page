d9be759672fcf7ee4efef48c12507d4bfe4a3c72
unknown <MSLadanov@gmail.com>
2023-08-07 14:36:04 +0300

    sign up  page creating

--- src/components/Routes/SignUp/SignUp.tsx
+++ src/components/Routes/SignUp/SignUp.tsx
@@ -1,7 +1,41 @@
-import React from 'react'
+import React from "react";
+import { Formik, Form, Field } from "formik";
+import * as Yup from "yup";
+import axios from "axios";
+
+const SignInSchema = Yup.object().shape({
+  email: Yup.string().email("Invalid email").required("Required"),
+  password: Yup.string().required("Required"),
+});
 
 export const SignUp = () => {
   return (
-    <div>SignUp</div>
+    <div>
+    {" "}
+    <h1>Sign Up</h1>
+    <hr />
+    <Formik
+      initialValues={{
+        email: "",
+        password: "",
+      }}
+      validationSchema={SignInSchema}
+      onSubmit={(values) => {
+        console.log(values)
+      }}
+    >
+      {({ errors, touched }) => (
+        <Form>
+          <Field name="email" />
+          {errors.email && touched.email ? <div>{errors.email}</div> : null}
+          <Field type="password" name="password" />
+          {errors.password && touched.password ? (
+            <div>{errors.password}</div>
+          ) : null}
+          <button type="submit">Sign Up</button>
+        </Form>
+      )}
+    </Formik>
+  </div>
   )
 }
