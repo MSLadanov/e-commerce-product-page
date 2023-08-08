5f9337ef071d02dc24ee25b458dc43f5bc264d82
unknown <MSLadanov@gmail.com>
2023-08-08 18:19:20 +0300

    sign up  page creating

--- src/components/Routes/SignUp/SignUp.tsx
+++ src/components/Routes/SignUp/SignUp.tsx
@@ -6,36 +6,47 @@ import axios from "axios";
 const SignInSchema = Yup.object().shape({
   email: Yup.string().email("Invalid email").required("Required"),
   password: Yup.string().required("Required"),
+  name: Yup.string().required("Required"),
+  surname: Yup.string().required("Required"),
 });
 
 export const SignUp = () => {
   return (
     <div>
-    {" "}
-    <h1>Sign Up</h1>
-    <hr />
-    <Formik
-      initialValues={{
-        email: "",
-        password: "",
-      }}
-      validationSchema={SignInSchema}
-      onSubmit={(values) => {
-        console.log(values)
-      }}
-    >
-      {({ errors, touched }) => (
-        <Form>
-          <Field name="email" />
-          {errors.email && touched.email ? <div>{errors.email}</div> : null}
-          <Field type="password" name="password" />
-          {errors.password && touched.password ? (
-            <div>{errors.password}</div>
-          ) : null}
-          <button type="submit">Sign Up</button>
-        </Form>
-      )}
-    </Formik>
-  </div>
-  )
-}
+      {" "}
+      <h1>Sign Up</h1>
+      <hr />
+      <Formik
+        initialValues={{
+          name: "",
+          surname: "",
+          img: "",
+          email: "",
+          password: "",
+        }}
+        validationSchema={SignInSchema}
+        onSubmit={(values) => {
+          console.log(values);
+        }}
+      >
+        {({ errors, touched }) => (
+          <Form>
+            <Field name="name" />
+            {errors.name && touched.name ? <div>{errors.name}</div> : null}
+            <Field name="surname" />
+            {errors.surname && touched.surname ? <div>{errors.surname}</div> : null}
+            <Field name="email" />
+            {errors.email && touched.email ? <div>{errors.email}</div> : null}
+            <Field name="img" type="file" />
+            {errors.img && touched.img ? <div>{errors.img}</div> : null}
+            <Field type="password" name="password" />
+            {errors.password && touched.password ? (
+              <div>{errors.password}</div>
+            ) : null}
+            <button type="submit">Sign Up</button>
+          </Form>
+        )}
+      </Formik>
+    </div>
+  );
+};
