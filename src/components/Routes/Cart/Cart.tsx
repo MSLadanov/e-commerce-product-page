340bbfdc8fca0910f5af77723108dcdb84bd72eb
unknown <MSLadanov@gmail.com>
2023-08-10 19:05:57 +0300

    cart  page creating

--- src/components/Routes/Cart/Cart.tsx
+++ src/components/Routes/Cart/Cart.tsx
@@ -1,7 +1,30 @@
 import React from 'react'
+import { useRef, useEffect } from "react";
+import { useSelector } from "react-redux";
+import { getCartData } from "../../../redux/slices/cartSlice";
+import { removeSneaker } from "../../../redux/slices/cartSlice";
+import { useDispatch } from "react-redux";
+import { getToken } from "../../../redux/slices/userSlice";
+import "./style.scss";
 
 export const Cart = () => {
+  const dropdownCartRef = useRef<HTMLInputElement>(null);
+  const cartData = useSelector(getCartData);
+  const dispatch = useDispatch();
+  const token = useSelector(getToken);
   return (
-    <div>Cart</div>
+    <div>{cartData.map((item: any) => (
+      <div key={item.cart_id}>
+        <img src={`http://localhost:3001/${item.img}`} alt="" />
+        <p>{item.brand + " " + item.name}</p>
+        <img
+          className="delete-icon"
+          onClick={() => dispatch(removeSneaker(item.cart_id))}
+          src="/images/icon-delete.svg"
+          alt="cart"
+        />
+      </div>
+    ))}
+  </div>
   )
 }
