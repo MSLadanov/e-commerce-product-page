b1bc53103d0f906bafae761784f075bb5565b9aa
Михаил Ладанов <MSLadanov@gmail.com>
2023-08-26 19:28:30 +0300

    create sneaker page creating

--- src/components/Routes/Orders/Orders.tsx
+++ src/components/Routes/Orders/Orders.tsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { getToken } from "../../../redux/slices/userSlice";
+import axios from "axios";
 
 export const Orders = () => {
     const token = useSelector(getToken);
+    const getOrdersInfo = async () => {
+      const info = (await axios.get(`http://localhost:3001/api/basket/`,{
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "multipart/form-data",
+        },
+      }))
+        .data as any;
+    };
+    useEffect(() => {
+      getOrdersInfo();
+    }, []);
     if(token === null){
         return (
             <div><h1>You must be signed for see link.</h1></div>
