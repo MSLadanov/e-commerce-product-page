81cd6a4c6b8d84408bf9c51708bc982fe0e43625
Михаил Ладанов <MSLadanov@gmail.com>
2023-08-30 18:24:27 +0300

    create sneaker page creating

--- src/components/Routes/Cart/Cart.tsx
+++ src/components/Routes/Cart/Cart.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from "axios";
 import { useRef, useEffect } from "react";
 import { useSelector } from "react-redux";
 import { getCartData } from "../../../redux/slices/cartSlice";
@@ -9,6 +10,7 @@ import "./style.scss";
 
 export const Cart = () => {
   const dropdownCartRef = useRef<HTMLInputElement>(null);
+  const [address, setAddress] = useState('')
   const cartData = useSelector(getCartData);
   const dispatch = useDispatch();
   const token = useSelector(getToken);
@@ -22,7 +24,22 @@ export const Cart = () => {
     });})
     let sum = cartData.reduce(function (acc : any, obj : any) { return acc + obj.price; }, 0);
     console.log(orderData)
+    console.log(address)
     console.log(sum)
+    // const order = axios.post(
+    //   "http://localhost:3001/api/basket/send",
+    //   JSON.stringify({
+    //     order:orderData,
+    //     address,
+    //     sum
+    //   }),
+    //   {
+    //     headers: {
+    //       Authorization: `Bearer ${token}`,
+    //       "Content-Type": "multipart/form-data",
+    //     },
+    //   }
+    // );
   }
   if (!cartData.length) {
     return (
@@ -33,20 +50,32 @@ export const Cart = () => {
     );
   }
   return (
-    <div>{cartData.map((item: any) => (
-      <div className='cart-item' key={item.cart_id}>
-        <img src={`http://localhost:3001/${item.img}`} alt="" />
-        <p>{item.brand + " " + item.name + ", " + " Size: " + item.size}</p>
-        <p>{item.price + " " + "$"}</p>
-        <img
-          className="delete-icon"
-          onClick={() => dispatch(removeSneaker(item.cart_id))}
-          src="/images/icon-delete.svg"
-          alt="cart"
-        />
-      </div>
-    ))}
-    {token !== null ?<button className='cart-btn' onClick={() => makeOrder()}>Make order</button>:<div className='center'><h1>You must be logged in to complete your order!</h1></div>}
-  </div>
-  )
+    <div>
+      {cartData.map((item: any) => (
+        <div className="cart-item" key={item.cart_id}>
+          <img src={`http://localhost:3001/${item.img}`} alt="" />
+          <p>{item.brand + " " + item.name + ", " + " Size: " + item.size}</p>
+          <p>{item.price + " " + "$"}</p>
+          <img
+            className="delete-icon"
+            onClick={() => dispatch(removeSneaker(item.cart_id))}
+            src="/images/icon-delete.svg"
+            alt="cart"
+          />
+        </div>
+      ))}
+      {token !== null ? (
+        <div>
+          <input type="text" placeholder='Input your address' onChange={(e) => setAddress(e.target.value)} />
+          <button className="cart-btn" onClick={() => makeOrder()}>
+            Make order
+          </button>
+        </div>
+      ) : (
+        <div className="center">
+          <h1>You must be logged in to complete your order!</h1>
+        </div>
+      )}
+    </div>
+  );
 }
