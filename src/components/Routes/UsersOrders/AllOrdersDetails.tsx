01651270123d1c4867350f15cd9a945f4c3f105c
Михаил Ладанов <MSLadanov@gmail.com>
2023-09-11 16:24:28 +0300

    orderspage creating

--- src/components/Routes/UsersOrders/AllOrdersDetails.tsx
+++ src/components/Routes/UsersOrders/AllOrdersDetails.tsx
@@ -1,54 +1,66 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 import { useSelector } from 'react-redux';
 import { getToken } from "../../../redux/slices/userSlice";
 import axios from 'axios';
 
 export const AllOrdersDetails = ({order} :any) => {
-    const [userInfo, setUserInfo] = useState({
-      name: '',
-      surname: '',
-      email: '',
-      img: ''
+  const ref = useRef(document.createElement("div"));
+  const [userInfo, setUserInfo] = useState({
+    name: "",
+    surname: "",
+    email: "",
+    img: "",
+  });
+  const token = useSelector(getToken);
+  const getFormattedDate = (date: any) => {
+    const formattedDate = date.split("T");
+    const datePart = formattedDate[0].match(/\d+/g),
+      year = datePart[0].substring(2),
+      month = datePart[1],
+      day = datePart[2];
+    return day + "." + month + "." + year;
+  };
+  const convertArray = (arr: any) => {
+    const convertedArray = JSON.parse(arr.replace("/"));
+    return convertedArray;
+  };
+  const deleteOrder = async (id: any) => {
+    const response = await axios({
+      method: "delete",
+      url: `http://localhost:3001/api/basket/delete/${id}`,
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    });
+  };
+  const getUserInfo = async (id: any) => {
+    const response = await axios({
+      method: "get",
+      url: `http://localhost:3001/api/user/info/${id}`,
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
     })
-    const token = useSelector(getToken);
-    const getFormattedDate = (date : any) => {
-        const formattedDate = date.split('T')
-        const datePart = formattedDate[0].match(/\d+/g),
-        year = datePart[0].substring(2),
-        month = datePart[1], day = datePart[2];
-        return day + "." + month + "." + year;
-      }
-      const convertArray = (arr:any) => {
-        const convertedArray = JSON.parse(arr.replace('/'))
-        return convertedArray
-      }
-      const deleteOrder = async (id : any) => {
-      const response = await axios({
-        method: "delete",
-        url: `http://localhost:3001/api/basket/delete/${id}`,
-        headers: {
-          "Content-Type": "application/json",
-          "Authorization": `Bearer ${token}`
-        },
-      });
-      }
-      const getUserInfo = async (id : any) => {
-        const response = await axios({
-          method: "get",
-          url: `http://localhost:3001/api/user/info/${id}`,
-          headers: {
-            "Content-Type": "application/json",
-            "Authorization": `Bearer ${token}`
-          },
-        }).then((res) => setUserInfo({...res.data})).catch((err) => console.log(err));
-        }
-        useEffect(() => {
-          getUserInfo(order.userId)
-        }, [])
-        
+      .then((res) => setUserInfo({ ...res.data }))
+      .catch((err) => console.log(err));
+  };
+  useEffect(() => {
+    getUserInfo(order.userId);
+  }, []);
+
+  const openDetailsDropdown = () => {
+    if(ref.current.style.height !== '0px'){
+      ref.current.style.height = '0px'
+    } else {
+      ref.current.style.height = ref.current.scrollHeight + 'px'
+    }
+  };
+
   return (
     <>
-      <div className="all-orders-card">
+      <div className="all-orders-card" onClick={() => openDetailsDropdown()}>
         {" "}
         <p>{order.id}</p>
         <p>{order.data.brand}</p>
@@ -69,14 +81,14 @@ export const AllOrdersDetails = ({order} :any) => {
           />
         </div>
       </div>
-      <div>
+      <div className="all-orders-details-container" ref={ref}>
         {convertArray(order.data).map((item: any, index: any) => {
           return (
             <div className="all-orders-cart-data-details" key={index}>
               <img src={`http://localhost:3001/${item.img}`} alt="" />
-                <p>{item.brand}</p>
-                <p>{item.name}</p>
-                <p>{item.size}</p>
+              <p>{item.brand}</p>
+              <p>{item.name}</p>
+              <p>{item.size}</p>
             </div>
           );
         })}
