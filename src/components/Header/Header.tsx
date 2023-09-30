e116ddd4697327d2dd4ca8b13f310d9f84153595
Михаил Ладанов <MSLadanov@gmail.com>
2023-09-30 18:13:16 +0300

    creating mobile version

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -78,6 +78,7 @@ export const Header = ({
           {token !== null && <NavLink to="orders/">My Orders</NavLink>}
           <NavLink to="about/">About</NavLink>
           <NavLink to="contact/">Contact</NavLink>
+          
         </div>
         <div className="account-navbar">
           <div
