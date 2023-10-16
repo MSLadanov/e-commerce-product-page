409f7e754e19e45bd692c2b9b7bdd7d4f09a3022
Михаил Ладанов <MSLadanov@gmail.com>
2023-10-16 18:44:01 +0300

    creating footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,6 +3,52 @@ import './style.scss'
 
 export const Footer = () => {
   return (
-    <div>Footer</div>
-  )
+    <>
+      <div className="content"></div>
+      <div className="footer">
+        <div className="footer-group">
+          <a className="footer-group-link" href="#">
+            About Us
+          </a>
+          <a className="footer-group-link" href="#">
+            Career
+          </a>
+          <a className="footer-group-link" href="#">
+            Privacy Policy
+          </a>
+          <a className="footer-group-link" href="#">
+            Partners
+          </a>
+        </div>
+        <div className="footer-group">
+          <a
+            className="social__link phone"
+            href="tel:88000999999"
+            target="_blank"
+          >
+            8 (800) 999 99 99
+          </a>
+          <a
+            className="social__link email"
+            href="mailto:info@sneaker.com"
+            target="_blank"
+          >
+            info@sneaker.com
+          </a>
+          <a
+            className="social__link vk"
+            href="https://vk.com/sneaker"
+            target="_blank"
+            title="Перейти в ВК"
+          ></a>
+          <a
+            className="social__link tg"
+            href="https://t.me/sneaker"
+            target="_blank"
+            title="Перейти в Телеграм"
+          ></a>
+        </div>
+      </div>
+    </>
+  );
 }
