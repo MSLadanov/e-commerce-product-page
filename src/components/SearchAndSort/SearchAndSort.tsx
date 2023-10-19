1fa24986ada59bb07ac99f2c0f254e5726c31b9c
Михаил Ладанов <MSLadanov@gmail.com>
2023-10-19 18:53:08 +0300

    creating searchbar

--- src/components/SearchAndSort/SearchAndSort.tsx
+++ src/components/SearchAndSort/SearchAndSort.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import { SearchBar } from './SearchBar/SearchBar'
+import { SortBar } from './SortBar/SortBar'
 import './style.scss'
 
 export const SearchAndSort = () => {
   return (
-    <div>SearchAndSort</div>
-  )
+    <div>
+      <SearchBar></SearchBar>
+      <SortBar></SortBar>
+    </div>
+  );
 }
