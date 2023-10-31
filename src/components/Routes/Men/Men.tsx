8d0561a2b03bb5ab294e90e9e2e3308fdbb5bf29
Михаил Ладанов <MSLadanov@gmail.com>
2023-10-31 20:27:47 +0300

    creating searchbar

--- src/components/Routes/Men/Men.tsx
+++ src/components/Routes/Men/Men.tsx
@@ -9,25 +9,29 @@ import "./style.scss";
 
 export default function Men() {
   const [data, setData] = useState([]);
+  const [initialData, setInitialData] = useState([])
   const sort = useSelector(getSortData);
   const search = useSelector(getSearchData);
   const getSneakers = async () => {
     const sneakers = (await axios.get("http://localhost:3001/api/sneaker/"))
       .data as any;
     setData(sneakers.filter((item: any) => item.sex === "MEN"));
+    setInitialData(sneakers.filter((item: any) => item.sex === "MEN"))
   };
   useEffect(() => {
     getSneakers();
   }, []);
   useEffect(() => {
+    setData(initialData)
     let filtered = data.filter((item : any) => {
-      return (item.name.toLowerCase().match(search.toLowerCase()))
-      // console.log(item.brand.toLowerCase().includes(search.toLowerCase()))
-          // return item.name.toLowerCase().includes(search.toLowerCase()) || item.brand.toLowerCase().includes(search.toLowerCase())
+      return item.name.toLowerCase().match(search.toLowerCase()) || item.brand.toLowerCase().match(search.toLowerCase())
   })
     setData(filtered)
     if(search.length === 0){
-      getSneakers()
+      setData(initialData)
+      if(sort.length !== 0){
+        gigaFilterFunction()
+      }
     }
   }, [search])
   useEffect(() => {
@@ -54,15 +58,15 @@ export default function Men() {
   if (search.length === 0 && sort.length === 0) {
     console.log("Do nothing...");
   }
-  if (search.length !== 0 && sort.length === 0) {
-    console.log("Searching...");
-    let filtered = data.filter((item : any) => {
-      return (item.name.toLowerCase().match(search.toLowerCase()))
-      // console.log(item.brand.toLowerCase().includes(search.toLowerCase()))
-          // return item.name.toLowerCase().includes(search.toLowerCase()) || item.brand.toLowerCase().includes(search.toLowerCase())
-  })
-  console.log(filtered)
-  }
+  // if (search.length !== 0 && sort.length === 0) {
+  //   console.log("Searching...");
+  //   let filtered = data.filter((item : any) => {
+  //     return (item.name.toLowerCase().match(search.toLowerCase()))
+  //     console.log(item.brand.toLowerCase().includes(search.toLowerCase()))
+  //         return item.name.toLowerCase().includes(search.toLowerCase()) || item.brand.toLowerCase().includes(search.toLowerCase())
+  // })
+  // console.log(filtered)
+  // }
   if (search.length !== 0 && sort.length !== 0) {
     console.log("Sorting and searching...");
   }
