9b634f0824c1ca1afa663a40c1c8a813d73a0bbe
unknown <MSLadanov@gmail.com>
2024-09-06 15:09:58 +0300

    Remove dumpscript

--- client/src/redux/slices/sortSlice.tsx
+++ client/src/redux/slices/sortSlice.tsx
@@ -1,22 +1,28 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+interface SortTypeState {
+  sort: {
+    type: string
+  }
+}
+
 export const sortSlice = createSlice({
   name: "sort",
   initialState: {
     type: '',
   },
   reducers: {
-    changeSort: (state: any, action) => {
+    changeSort: (state: {type: string}, action) => {
       state.type = action.payload;
     },
-    clearSort: (state: any) => {
+    clearSort: (state: {type: string}) => {
         state.type = '';
       },
   },
 });
 
 
-export const getSortData = (state: any) => state.sort.type;
+export const getSortData = (state: SortTypeState) => state.sort.type;
 
 export const { changeSort, clearSort } = sortSlice.actions;
 
