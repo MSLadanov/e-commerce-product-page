77655d164a08a355c12264e999520e4b9b91c79a
Михаил Ладанов <MSLadanov@gmail.com>
2024-09-07 10:13:58 +0300

    Dumpscript fix

--- client/src/redux/slices/searchSlice.tsx
+++ client/src/redux/slices/searchSlice.tsx
@@ -1,22 +1,28 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+interface Search {
+  search: {
+    text : string,
+  }
+}
+
 export const searchSlice = createSlice({
   name: "search",
   initialState: {
     text: '',
   },
   reducers: {
-    changeSearch: (state: any, action) => {
+    changeSearch: (state: {text : string}, action) => {
       state.text = action.payload;
     },
-    clearSearch: (state: any) => {
+    clearSearch: (state: { text : string}) => {
         state.text = '';
       },
   },
 });
 
 
-export const getSearchData = (state: any) => state.search.text;
+export const getSearchData = (state: Search) => state.search.text;
 
 export const { changeSearch, clearSearch } = searchSlice.actions;
 
