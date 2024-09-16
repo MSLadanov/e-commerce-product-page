30271bcc8beeca91ff3123f570de52546374da8f
Михаил Ладанов <MSLadanov@gmail.com>
2024-09-16 19:47:15 +0300

    remove dumpscript

--- client/src/hooks/useModal.jsx
+++ client/src/hooks/useModal.jsx
@@ -1,12 +1,16 @@
-function useModal(){
+import { useState } from "react"
+import { createPortal } from "react-dom"
 
+function useModal(){
+    const [ openModal, setOpenModal ] = useState(true)
     function toggleModal(){
 
     }
 
     function Modal(){
-
-    }
+        if (openModal){
+            return createPortal(<div>Modal</div>, document.getElementById('modal'))
+    }}
     return { toggleModal, Modal}
 }
 
