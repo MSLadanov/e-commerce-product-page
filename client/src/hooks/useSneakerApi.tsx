411a9973a761394e8d50143a3759c13546e8f154
Михаил Ладанов <MSLadanov@gmail.com>
2024-09-28 18:52:10 +0300

    remove dumpscript

--- client/src/hooks/useSneakerApi.tsx
+++ client/src/hooks/useSneakerApi.tsx
@@ -1,4 +1,9 @@
+import { useState } from "react";
+const BASE_URL = "http://localhost:3001/api";
+
 function useSneakerApi(){
+    const [isLoading, setIsLoading] = useState(false);
+    const [isError, setIsError] = useState(false);
     async function getSneakers() {
         
     }
