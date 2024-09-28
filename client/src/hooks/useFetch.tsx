e7ac4c254144aaab8c29a8e1627e428c630c007c
Михаил Ладанов <MSLadanov@gmail.com>
2024-09-28 20:39:10 +0300

    remove dumpscript

--- client/src/hooks/useFetch.tsx
+++ client/src/hooks/useFetch.tsx
@@ -5,6 +5,23 @@ function useFetch(url: string) {
   const [data, setData] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
   const [isError, setIsError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('')
+  async function fetchData (){
+    setIsLoading(true)
+    try {
+        const res = await axios.get(url)
+        setData(res.data)
+    } catch (error) {
+        setIsError(true)
+        setErrorMessage('')
+    } finally {
+        setIsLoading(false)
+    }
+  }
+  useEffect(() => {
+    fetchData()
+  },[])
+  return { data, isLoading, isError, errorMessage }
 }
 
 export default useFetch;
