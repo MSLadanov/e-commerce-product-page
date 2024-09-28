cd0fb5e6f461d29a124db300b9aa319e5f16ddee
Михаил Ладанов <MSLadanov@gmail.com>
2024-09-28 19:22:24 +0300

    remove dumpscript

--- client/src/hooks/useFetch.tsx
+++ client/src/hooks/useFetch.tsx
@@ -1,7 +1,10 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect } from "react";
+import axios from "axios";
 
-function useFetch(url : string){
-
+function useFetch(url: string) {
+  const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
 }
 
-export default useFetch
+export default useFetch;
