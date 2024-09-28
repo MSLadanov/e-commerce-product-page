a6a87eff41b53f4a5c162d86dfcd246856d4a793
Михаил Ладанов <MSLadanov@gmail.com>
2024-09-28 18:58:49 +0300

    remove dumpscript

--- client/src/hooks/useSneakerApi.tsx
+++ client/src/hooks/useSneakerApi.tsx
@@ -1,11 +1,19 @@
 import { useState } from "react";
+import axios from "axios";
+
 const BASE_URL = "http://localhost:3001/api";
 
 function useSneakerApi(){
     const [isLoading, setIsLoading] = useState(false);
     const [isError, setIsError] = useState(false);
     async function getSneakers() {
-        
+        try {
+            const response = await axios.get(`${BASE_URL}/sneaker`);
+            console.log(response.data);
+          } catch (error) {
+            console.error('Fetch error:', error);
+            throw error; 
+          }
     }
     return { getSneakers }
 }
