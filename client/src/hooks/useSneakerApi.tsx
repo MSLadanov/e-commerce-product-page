79609f0a05043d08c5479c210546974c87a0ee47
Михаил Ладанов <MSLadanov@gmail.com>
2024-09-28 19:01:50 +0300

    remove dumpscript

--- client/src/hooks/useSneakerApi.tsx
+++ client/src/hooks/useSneakerApi.tsx
@@ -3,13 +3,21 @@ import axios from "axios";
 
 const BASE_URL = "http://localhost:3001/api";
 
+interface Sneaker {
+    sex: string
+    name: string,
+    brand: string,
+    price: number,
+    discount: number
+  }
+
 function useSneakerApi(){
     const [isLoading, setIsLoading] = useState(false);
     const [isError, setIsError] = useState(false);
-    async function getSneakers() {
+    async function getSneakers(param : 'Men' | 'Women') {
         try {
             const response = await axios.get(`${BASE_URL}/sneaker`);
-            console.log(response.data);
+            console.log(response.data.filter((item: Sneaker) => item.sex === param));
           } catch (error) {
             console.error('Fetch error:', error);
             throw error; 
