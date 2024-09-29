cee5d7a20244c58e0f4591b7edbd7552d2ab9027
Михаил Ладанов <MSLadanov@gmail.com>
2024-09-29 16:48:56 +0300

    remove dumpscript

--- client/src/hooks/useSorting.tsx
+++ client/src/hooks/useSorting.tsx
@@ -1,7 +1,32 @@
 import { useState, useEffect } from "react"
 
+interface Sneaker {
+    sex: string;
+    name: string;
+    brand: string;
+    price: number;
+    discount: number;
+  }
+  
 function useSorting(initialData : [], sortingType : string){
-
+    const [sortedData, setSortedData] = useState(initialData)
+    useEffect(() => {
+        switch (sortingType) {
+            case "price_up":
+              sortedData.sort((a: Sneaker, b: Sneaker) => b.price - a.price);
+              break;
+            case "discount_up":
+              sortedData.sort((a: Sneaker, b: Sneaker) => b.discount - a.discount);
+              break;
+            case "discount_down":
+              sortedData.sort((a: Sneaker, b: Sneaker) => a.discount - b.discount);
+              break;
+            case "price_down":
+              sortedData.sort((a: Sneaker, b: Sneaker) => a.price - b.price);
+              break;
+          }
+    }, [sortingType])
+    return { sortedData }
 }
 
 export default useSorting
